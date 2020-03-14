cd1e53467058064b14a8ca058e8f0924b57e32dd
kelly <keehgiulianne@gmail.com>
2020-03-13 22:51:39 -0300

    aula dia1303

---
+++ teste/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::resource('/categories','CategoryController');
+
+
+//Route:: método_HTTP('url', 'controller@metodo');
